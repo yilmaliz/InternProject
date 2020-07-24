3f4f51d2c2af94c48ad9110a2d0c5e8809cea00d
Ali Yılmaz <yilmaliz96@gmail.com>
2020-07-24 10:04:12 +0300

    Revert "GenericView Added"
    This reverts commit 783f3923

--- Tweet/urls.py
+++ Tweet/urls.py
@@ -1,22 +1,9 @@
 
-from django.urls import path,include
-from .views import  TweetViewSet
-#tweet_list,tweet_detail,TweetAPIView,TweetDetails,GenericAPIViewDetail,GenericAPIViewList,
-from rest_framework.routers import DefaultRouter
-
-
-router = DefaultRouter()
-router.register('tweet', TweetViewSet, basename='tweet')
+from django.urls import path
+from .views import tweet_list,tweet_detail
 
 urlpatterns = [
-    path('viewset/',include(router.urls)),
-    path('viewset/<int:pk>/',include(router.urls)),
+    path('tweet/', tweet_list),
+    path('detail/<int:pk>/',tweet_detail)
 
-
-    # #path('tweet/', tweet_list),
-    # path('tweet/', TweetAPIView.as_view()),
-    # #path('detail/<int:pk>/',tweet_detail)
-    # path('detail/<int:id>/',TweetDetails.as_view()),
-    # path('generic/tweet/', GenericAPIViewList.as_view()),
-    # path('generic/tweet/<int:id>/', GenericAPIViewDetail.as_view()),
 ]
